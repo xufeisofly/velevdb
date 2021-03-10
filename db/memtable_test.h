a2b08d085703e6085b4d4821637aa6be3505c57a
xufei01 <xufei01@zhihu.com>
2021-03-10 13:37:52 +0800

    fix

--- db/memtable_test.h
+++ db/memtable_test.h
@@ -12,12 +12,18 @@ TEST(MemTableTest, AddandGet) {
   MemTable memtable(cmp);
 
   memtable.Add(1000, kTypeValue, "key1", "value1");
+  memtable.Add(1001, kTypeValue, "key1", "value2");
   std::string value;
   LookupKey lukey("key1", 1000);
   bool r = memtable.Get(lukey, &value);
 
   ASSERT_EQ(true, r);
   ASSERT_EQ("value1", value);
+
+  LookupKey lukey2("key1", 1001);
+  bool r2 = memtable.Get(lukey2, &value);
+  ASSERT_EQ(true, r2);
+  ASSERT_EQ("value2", value);
 }
 
 }
